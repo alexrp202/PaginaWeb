dc44b5e48466eed6f63874892a91890e49cdde93
alexrp202 <alexrp202@gmail.com>
2020-10-28 17:20:07 +0100

    navbar sin logo
    soy bueno v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: 'header', component: HeaderComponent },
+  { path: 'inicio', component: InicioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
